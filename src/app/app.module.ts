51fed999b14462c92f16d1d9fccd0fc74ee30426
Piotr Furmanek <p.furmanek@bboxx.co.uk>
2020-08-25 15:10:50 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app.routing';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { PostComponent } from './posts/post/post.component';
+import { UserComponent } from './users/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostsComponent,
+    UsersComponent,
+    PostComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {HomeComponent} from './home/home.component';
+import {PostsComponent} from './posts/posts.component';
+import {UsersComponent} from './users/users.component';
+
+export const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '/posts', component: PostsComponent},
+  {path: '/users', component: UsersComponent},
+  // {path: 'login', component: UserLoginComponent, canActivate: [NavigationGuard]},
+  // {
+  //   path: 'admin',
+  //   loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+  //   canActivate: [AuthGuard, NavigationGuard],
+  //   data: { preload: false }
+  // },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, {
+    useHash: true,
+    // preloadingStrategy: CustomPreloadingStrategyService
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
